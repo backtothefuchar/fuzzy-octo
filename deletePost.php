7c839b91acf05094144ef5ac8f0e5e8626b2ea45
backtothefuchar <79161539+backtothefuchar@users.noreply.github.com>
2021-05-04 00:35:54 -0500

    quick fizes

--- deletePost.php
+++ deletePost.php
@@ -2,19 +2,22 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-$con = mysqli_connect("mysql.eecs.ku.edu", "kaiwidman", "xeeves9e",
-"kaiwidman");
-if (!$con){
-    die("ERROR: Could not connect. ".mysqli_connect_error());
+$mysqli = new mysqli("mysql.eecs.ku.edu", "charansuresh", "Pe9hae4i", "charansuresh");
+
+if($mysqli->connect_errno){
+    printf("Connect failed: %s\n", $mysql->connect_errno);
+    exit();
+}	
+
+
+if(!empty($_POST['posts'])) {
+	foreach($_POST['posts'] as $post) {
+		$query = "DELETE FROM Posts WHERE post_id='" . $post . "'";
+		if ($result = $mysqli->query($query)) {
+			echo "<p>Deleted post with ID " . $post . "</p>";
+		}
+	}
 }
 
-$records = $_POST["post"];
-echo "The following post ID's have been deleted:<br>";
-
-foreach($records as $post_id)
-{
-    mysqli_query($con, "DELETE FROM POSTS WHERE post_id = '$post_id'");
-    echo "$post_id<br>";
-}
 	
 ?>
